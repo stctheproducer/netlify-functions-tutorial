fbf4017687c5de037482e246173ece46c9deea0e
Chanda Mulenga <dev@stctheproducer.com>
2021-09-20 23:43:41 +0200

    feat(custom-request): Customise function requests

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,16 @@
 /** @format */
 const axios = require('axios').default
 
-exports.handler = async function () {
-  const POKE_API = `https://pokeapi.co/api/v2/pokedex/kanto`
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`
 
   const { data } = await axios.get(POKE_API)
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries,
+    }),
   }
 }
